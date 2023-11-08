9c7b159a83d4c9bbe5a28bfd7d3c1464ceca0061
Ian-Flury <ianflury123@gmail.com>
2023-11-07 16:38:16 -0800

    rearranged project structure

---
+++ blinky/blinky_inst.v
@@ -0,0 +1,7 @@
+	blinky u0 (
+		.clk_clk                             (<connected-to-clk_clk>),                             //                          clk.clk
+		.reset_reset_n                       (<connected-to-reset_reset_n>),                       //                        reset.reset_n
+		.switches_external_connection_export (<connected-to-switches_external_connection_export>), // switches_external_connection.export
+		.leds_external_connection_export     (<connected-to-leds_external_connection_export>)      //     leds_external_connection.export
+	);
+
