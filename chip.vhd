42d7b6dfe7934ebfb7925a8a8f62e3c2a9d6f649
Ian-Flury <ianflury123@gmail.com>
2023-11-07 17:49:03 -0800

    format chip.vhd

--- chip.vhd
+++ chip.vhd
@@ -1,51 +1,53 @@
 --
 -- top level (wrapper)
 --
-library ieee;
-use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
-entity chip is
-port (
-clk: in std_logic;
-reset_n: in std_logic;
-sw : in std_logic;
-switches: in std_logic_vector(1 downto 0);
-leds: out std_logic_vector(1 downto 0);
-rled: out std_logic
-);
-end entity chip;
-architecture rtl_and_struct of chip is
-signal rled_tmp : std_logic;
-component blinky is
-port (
-clk_clk : in std_logic := 'X'; -- clk
-reset_reset_n : in std_logic := 'X'; -- reset_n
-leds_external_connection_export : out std_logic_vector(1 downto 0); -- export
-switches_external_connection_export : in std_logic_vector(1 downto 0) := (others => 'X') -- export
-);
-end component blinky;
-begin
-u0 : component blinky
-port map (
-clk_clk => clk,
-reset_reset_n => reset_n,
-leds_external_connection_export => leds,
-switches_external_connection_export => switches
-);
--- begin custom hardware
--- turning on red LED when the most left switch is up
-process (reset_n, clk)
-begin
-if reset_n = '0' then
-rled_tmp <= '0';
-elsif rising_edge(clk) then
-if (sw = '1') then
-rled_tmp <= '1';
-else
-rled_tmp <= '0';
-end if;
-end if;
-end process;
-rled <= rled_tmp;
--- end custom hardware
-end architecture rtl_and_struct;
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+USE ieee.numeric_std.ALL;
+
+ENTITY chip IS
+    PORT (
+        clk : IN STD_LOGIC;
+        reset_n : IN STD_LOGIC;
+        sw : IN STD_LOGIC;
+        switches : IN STD_LOGIC_VECTOR(1 DOWNTO 0);
+        leds : OUT STD_LOGIC_VECTOR(1 DOWNTO 0);
+        rled : OUT STD_LOGIC
+    );
+END ENTITY chip;
+
+ARCHITECTURE rtl_and_struct OF chip IS
+    SIGNAL rled_tmp : STD_LOGIC;
+    COMPONENT blinky IS
+        PORT (
+            clk_clk : IN STD_LOGIC := 'X'; -- clk
+            reset_reset_n : IN STD_LOGIC := 'X'; -- reset_n
+            leds_external_connection_export : OUT STD_LOGIC_VECTOR(1 DOWNTO 0); -- export
+            switches_external_connection_export : IN STD_LOGIC_VECTOR(1 DOWNTO 0) := (OTHERS => 'X') -- export
+        );
+    END COMPONENT blinky;
+BEGIN
+    u0 : COMPONENT blinky
+        PORT MAP(
+            clk_clk => clk,
+            reset_reset_n => reset_n,
+            leds_external_connection_export => leds,
+            switches_external_connection_export => switches
+        );
+    -- begin custom hardware
+    -- turning on red LED when the most left switch is up
+    PROCESS (reset_n, clk)
+    BEGIN
+        IF reset_n = '0' THEN
+            rled_tmp <= '0';
+        ELSIF rising_edge(clk) THEN
+            IF (sw = '1') THEN
+                rled_tmp <= '1';
+            ELSE
+                rled_tmp <= '0';
+            END IF;
+        END IF;
+    END PROCESS;
+    rled <= rled_tmp;
+    -- end custom hardware
+END ARCHITECTURE rtl_and_struct;
