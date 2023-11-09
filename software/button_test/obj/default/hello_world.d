7593eaa3b67bee263977f0b6098358fa228d0121
Ian-Flury <ianflury123@gmail.com>
2023-11-09 12:34:27 -0800

    added buttons works. Ready to merge

---
+++ software/button_test/obj/default/hello_world.d
@@ -0,0 +1,17 @@
+obj/default/hello_world.o: hello_world.c \
+ ../button_test_bsp//HAL/inc/sys/alt_stdio.h ../button_test_bsp/system.h \
+ ../button_test_bsp/linker.h \
+ ../button_test_bsp//drivers/inc/altera_avalon_pio_regs.h \
+ ../button_test_bsp//HAL/inc/io.h ../button_test_bsp//HAL/inc/alt_types.h
+
+../button_test_bsp//HAL/inc/sys/alt_stdio.h:
+
+../button_test_bsp/system.h:
+
+../button_test_bsp/linker.h:
+
+../button_test_bsp//drivers/inc/altera_avalon_pio_regs.h:
+
+../button_test_bsp//HAL/inc/io.h:
+
+../button_test_bsp//HAL/inc/alt_types.h:
