e1ed0f1524afe43175dff5b7c2986607d60b4259
joeymacauley02 <joeymacauley02@gmail.com>
2023-11-28 17:13:08 -0800

    phase 1 of building out blackjack functions

--- software/blinky-sw/blinky.c
+++ software/blinky-sw/blinky.c
@@ -4,10 +4,10 @@
 #include <stdio.h>
 
 
-char cardValues[52] = {'2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K','A',
-					   '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A',
-					   '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A',
-					   '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'};
+char cardValues[52] = {'2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K','A',
+					   '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A',
+					   '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A',
+					   '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A'};
 int cardDeck[52] = {0};
 int bankRoll = 1000;
 int dealerSum = 0; 
@@ -42,9 +42,10 @@ void updateBankroll(int, int);
 
 
 // Game Functionality
-int GenerateRandomCard(void); // Hardware Random Number Generator
-void updateDeck(void); // Array of Flags - Set Flag when card is selected
+int generateRandomCard(void); // Hardware Random Number Generator
+void updateDeck(int); // Array of Flags - Set Flag when card is selected
 void resetDeck(void); 
+int translateCardValue(int); 
 void displayPlayerSum(int); // seven segment HEX7 and HEX6
 void displayDealerSum(int); // seven segment HEX5 and HEX4
 void displayResult(int);
@@ -53,7 +54,6 @@ void displayResult(int);
 // Player Actions
 void hit(void); // KEY3
 void stay(void); // KEY2
-void submitBet(void); // KEY0
 
 
 int main()
@@ -77,10 +77,8 @@ int main()
 //			IOWR_ALTERA_AVALON_PIO_DATA(SEV_SEG_0_BASE, random_num);
 
 
-
-
 			char msg[10];
-			card_val = GenerateRandomCard();
+			card_val = generateRandomCard();
 			itoa(card_val, msg, 10);
 			alt_putstr(msg);
 			alt_putstr("\n");
@@ -151,13 +149,31 @@ void gameInitialization(void) {
 	gamesPlayed = 0;  
 }  
 void playerBet(void) {
-	// Read Switches once KEY0 is pressed
+	// Wait for KEY0 to be pressed
+	// Read Values on Switch
+	// Translate Values to DEC.
 	return;
 } 
 void dealInitialCards(void) {
-	// Generate Dealer First Card and Player Cards
-	// Update and Display Dealer Sum
-	// Update and Display Player Sum
+	// Dealer First Card Shown
+	int tempCard = generateRandomCard(); 
+	while(cardDeck[tempCard] == 1) {
+		tempCard = generateRandomCard(); 
+	}
+	updateDeck(tempCard);
+	int cardValue = translateCardValue(cardValues[tempCard]);
+	dealerSum = dealerSum + cardValue; 
+
+	// Player First Two Cards
+	for (int i = 0; i < 2; i++) {
+		tempCard = generateRandomCard(); 
+		while(cardDeck[tempCard] == 1) {
+			tempCard = generateRandomCard(); 
+		}
+		updateDeck(tempCard);
+		cardValue = translateCardValue(cardValues[tempCard]);
+		playerSum = dealerSum + cardValue;
+	}
 	return;
 } 
 void playerTurn(void) {
@@ -170,6 +186,9 @@ void playerTurn(void) {
 int playerBust(int playerSum) {
 	// Check if Player sum <= 21
 	int busted = 0;
+	if (playerSum > 21) {
+		busted = 1; 
+	}
 	return busted;
 } 
 void dealerTurn(void) {
@@ -180,9 +199,21 @@ void dealerTurn(void) {
 int dealerBust(int dealerSum) {
 	// Check if Dealer sum <= 21
 	int busted = 0;
+	if (dealerSum > 21) {
+		busted = 1; 
+	}
 	return busted;
 } 
 void updateBankroll(int busted, int playerBet) {
+	if (busted == 1) {
+		if (playerBet >= bankRoll) {
+			// TODO: GAME OVERRRRRRRRRRRRR
+		}
+		bankRoll = bankRoll - playerBet; 
+	}
+	else {
+		bankRoll = bankRoll + playerBet; 
+	}
 	return; 
 } 
 
@@ -190,18 +221,29 @@ void updateBankroll(int busted, int playerBet) {
 /************************************************************
  Game Functionality
 *************************************************************/ 
-int GenerateRandomCard(void) {
+int generateRandomCard(void) {
 	int random_num = IORD_ALTERA_AVALON_PIO_DATA(RANDOMS_BASE);
 	return abs(random_num %52);
 } 
-void updateDeck(void) {
+void updateDeck(int cardIdx) {
 	// set 0 -> 1 in cardDeck array
+	cardDeck[cardIdx] = 1; 
 	return;
 } 
 void resetDeck(void) {
 	// set all indicies of cardDeck array back to 0
+	for (int i = 0; i < 52; i++) {
+		cardDeck[i] = 0; 
+	}
 	return;
 } 
+int translateCardValue(int cardIdx) {
+	int cardValue = (int)(cardValues[cardIdx] - '0'); 
+	if (cardValue > 11) {
+		cardValue = 10; 
+	}
+	return cardValue; 
+}
 void displayPlayerSum(int playerSum) {
 	// display sum to HEX7 and HEX6 on seven seg display
 	return;
@@ -212,6 +254,12 @@ void displayDealerSum(int dealerSum) {
 } 
 void displayResult(int result) {
 	// display message on LCD
+	if (result == 1) { // WIN
+		return; 
+	}
+	else { // LOSE
+		return; 
+	}
 	return;
 }
 
@@ -221,18 +269,21 @@ void displayResult(int result) {
 *************************************************************/ 
 void hit(void) {
 	// Generate Random Card
-	// Update Deck
+	int card = generateRandomCard(); 
+
+	// Check and Update Deck
+	while(cardDeck[card] == 1) {
+		card = generateRandomCard(); 
+	}
+	updateDeck(card);
+
+	// Char to Int
+	int cardValue = translateCardValue(cardValues[card]);
+
 	// Update playerSum
+	playerSum = playerSum + cardValue;
 	return;
 } 
-void stay(void) {
-	// Dealer Turn
+void stay(void) { // Nothing Happens
 	return;
 }
-void submitBet(void) {
-	// ReadSwitches
-	// KEY0 Press for Enter
-	return;
-}
-
-
